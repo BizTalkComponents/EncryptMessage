fb7abb76054c37f2db27f8c2f1264b1e6da28284
robinhultman <robin.hultman@gmail.com>
2015-09-15 14:57:05 +0200

    Bumped version

--- Src/EncryptMessage/Properties/AssemblyInfo.cs
+++ Src/EncryptMessage/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.9.0-beta")]
+[assembly: AssemblyInformationalVersion("0.9.1-beta")]
